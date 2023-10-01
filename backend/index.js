d103e37507acb6e3e17b989b752e2d14f1f8b926
vivekkushal <vivekjss1@gmail.com>
2023-10-01 19:50:08 +0530

    chore: Add comments

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@
 // dotenv is a package that loads environment variables from a dotenv file into the process.env Object available to us globally in a nodejs environment
 
 // config method will attach the environment variables to the process.env Object
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const cors = require("cors"); // for production
-const mongoose = require("mongoose");
-const workoutRoutes = require("./routes/workouts");
+const express = require('express');
+const cors = require('cors'); // for production
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
 
-// creates express app
+// creates an express app
 const app = express();
 
 // middleware - any code that executes between us getting a request on the server and us sending a response
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 // route handler which reacts to requests
 // to only fire these routes when we come to a specific path, we should pass the first argument
-app.use("/api/workouts", workoutRoutes); // see the comment on workouts.js
+app.use('/api/workouts', workoutRoutes); // see the comment on workouts.js
 // app.get("/", (req, res) => {
 //   res.json({ mssg: "Welcome to the app" });
 // });
@@ -37,7 +37,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: "tnn-mern", // by this name a db will be created in Atlas
+    dbName: 'tnn-mern', // by this name a db will be created in Atlas
   })
   .then(() => {
     // listen for requests only once we are connected to db
